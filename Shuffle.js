598df3de7871ced03ef2331010779dfdd5a08034
Chaz Eichenmiller <chazeichenmiller@gmail.com>
2026-03-04 21:26:25 -0500

    please just work again

--- Shuffle.js
+++ Shuffle.js
@@ -1,4 +1,4 @@
-export function shuffle(array) {
+export function Shuffle(array) {
 	for (let i = array.length - 1; i >= 1; i--) {
 		const j = Math.floor(Math.random() * (i + 1));
 		[array[i], array[j]] = [array[j], array[i]];
