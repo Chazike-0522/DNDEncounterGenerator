1a68996ef6065e5c43cd3921f283655a17f7753a
Chaz Eichenmiller <chazeichenmiller@gmail.com>
2026-03-05 19:33:29 -0500

    put a carousel on that ish

---
+++ Track.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded",() => {
+
+const track = document.querySelector(".track");
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelectorAll(".next");
+const prevBtn = document.querySelectorAll(".prev");
+
+let index =0;
+const total = slides.length;
+
+track.style.transform = `translateX(-${index * 100}%)`;
+
+nextBtn.forEach(btn => btn.addEventListener("click", () => {
+	index++;
+	if (index >= total) {
+		index = 0;
+	}
+	track.style.transform = `translateX(-${index * 100}%)`;
+}));
+
+prevBtn.forEach(btn => btn.addEventListener("click", () => {
+	index--;
+	if (index < 0) {
+		index = total - 1;
+	}
+	track.style.transform = `translateX(-${index * 100}%)`;
+}));
+});
